fec19fb45cbf145fb5d335e33aaaeee6f5869cb5
Paul Eaton <paul.eaton@subdere.gov.cl>
2022-12-16 10:14:47 -0300

    Creating model Projects and migrating

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Project(models.Model):
+    project_name = models.CharField(max_length=200)
+    id_subdere = models.CharField(max_length=200)
+    project_description = models.TextField()
+
+    pub_date = models.DateTimeField('date published')
+
+
