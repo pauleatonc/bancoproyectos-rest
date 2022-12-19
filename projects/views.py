8a8a07154a1976720f708be99bd58322d850f663
Paul Eaton <paul.eaton@subdere.gov.cl>
2022-12-19 16:58:24 -0300

    crating views like blocks

--- projects/views.py
+++ projects/views.py
@@ -6,14 +6,13 @@ from .models import Project
 
 
 def index(request):
-    return HttpResponse("Banco de Proyectos %s.")
-
-
-def search(request):
     latest_project_list = Project.objects.order_by('-pub_date')[:5]
     context = {'latest_project_list': latest_project_list}
     return render(request, 'projects/index.html', context)
 
+def search(request):
+
+    return render(request, 'projects/search.html')
 
 def project(request, project_id):
     return HttpResponse("Proyecto específico: %s." % project_id)
