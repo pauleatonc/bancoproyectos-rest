864dcab68e71ddf687e83c82ff948c972f08506c
Paul Eaton <paul.eaton@subdere.gov.cl>
2023-05-18 10:05:54 -0400

    FIX: Merging branches into master

--- applications/regioncomuna/views.py
+++ applications/regioncomuna/views.py
@@ -52,5 +52,6 @@ def get_comunas(request):
     return JsonResponse(list(comunas), safe=False)
 
 # cargar los datos del archivo JSON en la base de datos si aún no se han cargado
+# Comentar para correr las migraciones.
 if not Region.objects.exists() and not Comuna.objects.exists() and os.path.exists('regioncomuna.json'):
     cargar_datos_archivo_json()
