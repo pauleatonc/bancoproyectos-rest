56fc6edb9c082b22fd166b8fd2c50910987060e9
JaimeHernandezE <jhearquitecto@gmail.com>
2023-06-05 23:57:15 -0400

    FIX: Formulario de contacto corregido

--- applications/home/views.py
+++ applications/home/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from applications.projects.models import Project, Program, Type
 from applications.home.models import Contact
 from applications.projects.views import ProjectsListView
+from django.core.mail import send_mail
 
 from django.views.generic import (
     View,
@@ -22,3 +23,21 @@ class ContactCreateView(CreateView):
     form_class = ContactForm
     model = Contact
     success_url = '.'
+
+    '''def form_valid(self, form):
+        # Guardar el formulario y obtener la instancia del modelo
+        self.object = form.save()
+
+        # Obtener el correo electrónico ingresado en el formulario
+        email = form.cleaned_data['email']
+
+        # Enviar el correo electrónico con los datos del formulario
+        send_mail(
+            'Copia de formulario de contacto',
+            form.cleaned_data['message'],
+            'jhearquitecto@gmail.com',  # Correo electrónico del remitente
+            [email],  # Correo electrónico del destinatario
+            fail_silently=False,
+        )
+
+        return super().form_valid(form)'''
