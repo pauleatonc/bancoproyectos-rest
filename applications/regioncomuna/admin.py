64768b6649b80d71cdbf7f546030a7089ca2eb12
JaimeHernandezE <jhearquitecto@gmail.com>
2023-06-07 16:03:00 -0400

    FIX: Agregado import / export al django admin

--- applications/regioncomuna/admin.py
+++ applications/regioncomuna/admin.py
@@ -1,6 +1,24 @@
 from django.contrib import admin
 from .models import Region, Comuna
+from import_export.admin import ImportExportMixin
+from import_export.resources import ModelResource
 
 
-admin.site.register(Region)
-admin.site.register(Comuna)
+class RegionResource(ModelResource):
+    class Meta:
+        model = Region
+
+
+class ComunaResource(ModelResource):
+    class Meta:
+        model = Comuna
+
+
+@admin.register(Region)
+class RegionAdmin(ImportExportMixin, admin.ModelAdmin):
+    resource_class = RegionResource
+
+
+@admin.register(Comuna)
+class ComunaAdmin(ImportExportMixin, admin.ModelAdmin):
+    resource_class = ComunaResource
