1bcef08421ccf30bd87c63e6ff0329dadcb57665
JaimeHernandezE <jhearquitecto@gmail.com>
2023-06-07 08:51:41 -0400

    FIX: Corregido inicio de sesión, redirecciona a la página desde donde se requirió

--- applications/home/views.py
+++ applications/home/views.py
@@ -62,3 +62,11 @@ class ContactCreateView(CreateView):
         )
 
         return super().form_valid(form)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['programs'] = Program.objects.all()
+        context['types'] = Type.objects.all()
+
+        return context
+
