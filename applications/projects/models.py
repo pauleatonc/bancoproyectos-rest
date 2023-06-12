314069c4823afc1a77d4fc1700c1b147d035ab07
vecarope <vecarope@mac-vcaro.local>
2023-06-12 14:34:25 -0400

    ADD: save img with new size

--- applications/projects/models.py
+++ applications/projects/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from imagekit.models import ImageSpecField
-from imagekit.processors import ResizeToFill
+from imagekit.models import ProcessedImageField
+from imagekit.processors import ResizeToFill, ResizeToFit
 from applications.regioncomuna.models import Region, Comuna
 from .managers import ProjectsManager
 from django.core.validators import FileExtensionValidator
@@ -54,7 +55,8 @@ class Type(models.Model):
     name = models.CharField(
         max_length=200, verbose_name='Tipo de Proyecto', unique=True)
     guides = models.ManyToManyField(Guide, related_name='guides')
-    icon_type = models.CharField(verbose_name='Icono ( Nombre icono)', max_length=200,default='other_admission')
+    icon_type = models.CharField(
+        verbose_name='Icono ( Nombre icono)', max_length=200, default='other_admission')
 
     def __str__(self):
         return self.name
@@ -66,18 +68,25 @@ class Year(models.Model):
     def __str__(self):
         return self.number
 
+
 class PrioritizedTag(models.Model):
-    prioritized_tag = models.CharField(max_length=20, verbose_name='Tag para proyectos priorizados', unique=True)
+    prioritized_tag = models.CharField(
+        max_length=20, verbose_name='Tag para proyectos priorizados', unique=True)
+
     def __str__(self):
         return self.prioritized_tag
 
+
 class ChecklistDocuments(models.Model):
-    documentname = models.CharField(verbose_name='Nombre documento', unique=True, max_length=50)
+    documentname = models.CharField(
+        verbose_name='Nombre documento', unique=True, max_length=50)
     description = models.TextField(verbose_name='Descripción documento')
-    file = models.ManyToManyField(Guide, verbose_name='Guías de referencia', blank=True)
+    file = models.ManyToManyField(
+        Guide, verbose_name='Guías de referencia', blank=True)
 
     def __str__(self):
         return self.documentname
+
 
 class Project(models.Model):
     name = models.CharField(
@@ -94,19 +103,19 @@ class Project(models.Model):
     public = models.BooleanField(default=True)
     video = models.CharField(max_length=200, null=True,
                              blank=True, verbose_name='Youtube')
-    portada = models.ImageField(upload_to='projects', null=True,
-                                blank=False, verbose_name='Foto miniatura (obligatorio)')
-    portacard = ImageSpecField(source='portada', processors=[
-        ResizeToFill(300, 300)], format='png', options={'quality': 60})
-    beforeimage = models.ImageField(
-        upload_to='projects', null=True, blank=True, verbose_name='Imagen Antes')
-    beforeimageresize = ImageSpecField(source='beforeimage', processors=[
-        ResizeToFill(800, 600)], format='png', options={'quality': 60})
-    afterimage = models.ImageField(
-        upload_to='projects', null=True, blank=True, verbose_name='Imagen Después')
-    afterimageresize = ImageSpecField(source='afterimage', processors=[
-        ResizeToFill(800, 600)], format='png', options={'quality': 60})
-
+    portada = ProcessedImageField(upload_to='projects', processors=[
+        ResizeToFill(1200, 630)], format='WEBP', options={'quality': 60}, null=True,
+        blank=False, verbose_name='Foto miniatura (obligatorio)')
+    beforeimage = ProcessedImageField(upload_to='projects',
+                                      processors=[ResizeToFit(1200, 630)],
+                                      format='WEBP',
+                                      options={'quality': 60},
+                                      null=True, blank=True, verbose_name='Imagen Antes')
+    afterimage = ProcessedImageField(upload_to='projects',
+                                     processors=[ResizeToFit(1200, 630)],
+                                     format='WEBP',
+                                     options={'quality': 60},
+                                     null=True, blank=True, verbose_name='Imagen Después')
     eett = models.FileField(upload_to='project_documents', validators=[
         FileExtensionValidator(['pdf'], message='Solo se permiten archivos PDF.'), validate_file_size_five],
         null=True, blank=False, verbose_name='EETT')
@@ -116,10 +125,10 @@ class Project(models.Model):
     comuna = models.ForeignKey(
         Comuna, on_delete=models.SET_NULL, null=True, blank=True, verbose_name='Comuna')
 
-    prioritized_tag = models.ManyToManyField(PrioritizedTag, blank=False, verbose_name = 'Tag proyecto priorizado')
+    prioritized_tag = models.ManyToManyField(
+        PrioritizedTag, blank=False, verbose_name='Tag proyecto priorizado')
 
-    #checklist = models.ManyToManyField(ChecklistDocuments, verbose_name='Checklist de documentos obligatorios')
-
+    # checklist = models.ManyToManyField(ChecklistDocuments, verbose_name='Checklist de documentos obligatorios')
 
     def get_comunas_by_region(self):
         if self.region:
@@ -132,18 +141,17 @@ class Project(models.Model):
 
 
 class Projectimage(models.Model):
-    image = models.ImageField(upload_to='projects')
-    imagethumbnail = ImageSpecField(source='image',
-                                    processors=[ResizeToFill(300, 300)],
-                                    format='png',
-                                    options={'quality': 60})
-    imagecarousel = ImageSpecField(source='image',
-                                   processors=[ResizeToFill(768, 500)],
-                                   format='png',
-                                   options={'quality': 60})
+    image_carousel = ProcessedImageField(upload_to='projects',
+                                         verbose_name='Galeria de imagenes',
+                                         processors=[ResizeToFit(1200, 630)],
+                                         format='WEBP',
+                                         options={'quality': 60})
 
     project = models.ForeignKey(
-        Project, null=False, blank=False, on_delete=models.CASCADE, related_name='images')
+        Project,
+        on_delete=models.CASCADE,
+        related_name='images'
+    )
 
 
 class Projectfile(models.Model):
