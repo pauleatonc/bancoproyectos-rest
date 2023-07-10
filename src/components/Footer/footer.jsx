e7411a0e69fadcd6b233718820800c52009343eb
Rosario Herrera Amunátegui <114428069+RosarioHA@users.noreply.github.com>
2023-07-10 14:33:39 -0400

    agrega estilos y direcciones a Footer

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -1,39 +1,41 @@
-//import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
     return (
       <footer className="content-always-on-display">
         <div className="container">
-          <div className="line"></div>
+
+          <div className="line w-sm-50"></div>
+
           <div className="row">
-            <div className="col-md-3 a11y-fonts-col-12"><img className="mw-100 mb-3" src="https://via.placeholder.com/170x80" />
-              <p>La alternativa para digitalizar los trámites de la ciudadanía.</p>
-              <p>Esta plataforma es administrada:</p><img className="mw-100 mb-3" src="https://via.placeholder.com/140x50" /><a className="py-0" href="www.digital.gob.cl">www.digital.gob.cl</a>
-            </div>
             <div className="col-md-3 a11y-fonts-col-12">
-              <div className="text-uppercase mb-3">Secciones</div><a href="#">Conjuntos de Datos</a><a href="#">Organizaciones</a><a href="#">Categorías</a><a href="#">Documentos</a><a href="#">Ayuda</a><a href="#">Iniciativas Destacadas</a>
+              <img className="mw-100 h-75 mb-3 img-fluid img-sm" src="src/static/img/logo_SubDesarrollo.png" />
             </div>
+
             <div className="col-md-3 a11y-fonts-col-12">
-              <div className="text-uppercase mb-3">Enlaces internos</div><a href="#">Políticas de Privacidad</a><a href="#">Normativas</a><a href="#">Términos y Condiciones</a><a href="#">Sitios relacinados</a><a href="#">Licenciamiento</a><a href="#">Notificar error</a>
+              <p className="mb-3">Secciones</p>
+              <Link to="/" className="link"> Inicio </Link>
+              <Link to="/bancodeproyectos" className="link"> Banco de Proyectos </Link>
             </div>
-            <div className="col-md-3 a11y-fonts-col-12">
-              <div className="text-uppercase mb-3">Ayuda institucional</div><a href="#">Ingreso de solicitud de ayuda</a><b className="d-block mt-3">Mesa de Ayuda</b>
-              <p className="mb-0">
-                Horario Lu - Vi / 9:00hrs -
-                18:00hrs
-              </p><a className="py-0" href="tel:6003970000">600 397 0000</a>
+
+            <div className="col-md-3 a11y-fonts-col-12 h-100 mt-5 mt-md-0">
+              <p className="mb-3">Enlaces de Interés</p>
+              <a className="link" href="https://www.subdere.gov.cl/programas/divisi%C3%B3n-municipalidades/programa-mejoramiento-urbano-y-equipamiento-comunal-pmu" target="_blank" rel="noreferrer">Programa Mejoramiento Urbano</a>
+              <a className="link" href="https://www.subdere.gov.cl/programas/divisi%C3%B3n-municipalidades/programa-mejoramiento-de-barrios-pmb" target="_blank" rel="noreferrer">Programa Mejoramiento de Barrios</a>
+              <a className="link text-wrap h-75" href="https://www.subdere.gov.cl/" target="_blank" rel="noreferrer">Subsecretaría de Desarrollo Regional y Administrativo SUBDERE</a>
+              <a className="link text-wrap h-75" href="https://www.interior.gob.cl/" target="_blank" rel="noreferrer">Ministerio del Interior y Seguridad Pública</a>
+            </div>
+
+            <div className="col-md-3 a11y-fonts-col-12 mt-5 mt-md-0">
+              <p className="mb-3">Ayuda</p>
+              <Link to="/contacto" className="link">Ingresa un Formulario de Contacto</Link>
+              <p className="mb-3 mt-md-5">Dirección:</p>
+              <p className="mb-3"> Teatinos 92 - Pisos 2 y 3. Santiago, Chile.</p>
+             
             </div>
           </div>
         </div>
-
-
-        <div className="my-3">
-  <button className="btn btn-primary btn-default-size" type="button">Visitas a La Moneda</button>
-</div>
-
-      </footer>
-
-      
+      </footer>  
     );
   };
   
