fe63ded9d72d83d24bb5e5ba7d53ee50dd63397c
Rosario Herrera Amunátegui <114428069+RosarioHA@users.noreply.github.com>
2023-07-11 16:09:29 -0400

    da estilo vista landing

--- src/components/Landing/buscadorproyectos.jsx
+++ src/components/Landing/buscadorproyectos.jsx
@@ -1,6 +1,6 @@
 const BuscadorProyectos = () => {
     return (
-        <div className="container col-md-4 d-flex flex-column my-md-5">
+        <div className="container col-md-4 d-flex flex-column my-md-4">
             <p className="text-center my-4">Accede al buscador de proyectos escribiendo palabras clave</p>
             <form className="search mx-auto col my-3 my-md-4" action="" method="GET">
                 <input className="form-control " type="text" name="search" placeholder="Texto demo" aria-label="Texto demo" aria-describedby="searchAction"/>
