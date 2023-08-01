d0e17540c963048beff30eefd04d16683092e6de
JaimeHernandezE <jhearquitecto@gmail.com>
2023-07-31 22:32:26 -0400

    ADD: CORS Configuration para aceptar API en local

--- projectbank/settings/base.py
+++ projectbank/settings/base.py
@@ -31,7 +31,7 @@ THIRD_PARTY_APPS = [
     'storages',
     'captcha',
     'rest_framework',
-
+    'corsheaders',
 ]
 
 INSTALLED_APPS = DJANGO_APPS + LOCAL_APPS + THIRD_PARTY_APPS
@@ -44,6 +44,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'projectbank.urls'
@@ -110,3 +111,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# cors authorization
+CORS_ALLOWED_ORIGINS = ['http://127.0.0.1:5173']
