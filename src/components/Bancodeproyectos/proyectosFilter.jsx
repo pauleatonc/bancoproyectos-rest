cbc8a5910875338ffda2c4e4f65a3d1917ac8dd8
Rosario Herrera Amunátegui <114428069+RosarioHA@users.noreply.github.com>
2023-08-02 17:35:15 -0400

    crea theme.scss y estilos de texto para vistas landing, bancodeproyectos y proyecto

--- src/components/Bancodeproyectos/proyectosFilter.jsx
+++ src/components/Bancodeproyectos/proyectosFilter.jsx
@@ -1,8 +1,7 @@
-import "../../static/styles/proyectosFilter.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTrashCan } from '@fortawesome/free-regular-svg-icons';
 import { useEffect, useState } from 'react';
-import Select from 'react-select';
+//import Select from 'react-select';
 
 import { getAllRegionComunas } from '../../api/RegionComuna/regioncomuna.api';
 
@@ -45,36 +44,36 @@ const FiltroProyectos = () => {
 
       <div className="mb-md-4" id="filter-container">
         <div className="container d-flex justify-content-between my-3 p-0">
-            <p className="font-level-3">Filtrar</p>
-            <button className="btn-limpiar p-2">
-                Limpiar filtro <FontAwesomeIcon icon={faTrashCan} />
-            </button>
+          <p className="text-sans-h3">Filtrar</p>
+          <button className="text-sans-p btn-limpiar p-2">
+            Limpiar filtro <FontAwesomeIcon icon={faTrashCan} />
+          </button>
         </div>
 
-        <p className="">¿En qué programa está el proyecto que buscas?</p>
-        <p>Puedes elegir más de uno.</p>
+        <p className="text-sans-p">¿En qué programa está el proyecto que buscas?</p>
+        <p className="text-sans-h5">Puedes elegir más de uno.</p>
 
         <div className="container d-flex justify-content-around mx-0 p-0">
-            <div className="col-md-2 d-flex flex-column align-items-center mr-5">
-                <div className="categorias-circle rounded-circle d-flex align-items-center justify-content-center my-md-3">
-                    <span className="categorias-siglas h4 font-weight-bold mt-2">PMU</span>
-                </div>
-                <p className="text-center font-weight-bold">Programa Mejoramiento Urbano</p>
+          <div className="col-md-2 d-flex flex-column align-items-center mr-5">
+            <div className="categorias-circle rounded-circle d-flex align-items-center justify-content-center my-md-3">
+              <span className="categorias-siglas h4 font-weight-bold mt-2">PMU</span>
             </div>
-            <div className="col-md-2 d-flex flex-column  align-items-center">
-                <div className="categorias-circle rounded-circle d-flex align-items-center justify-content-center my-md-3">
-                    <span className="categorias-siglas h4 font-weight-bold mt-2">PMB</span>
-                </div>
-                <p className="text-center font-weight-bold">Programa Mejoramiento de Barrios</p>
+            <p className="text-sans-h5-bold text-center">Programa Mejoramiento Urbano</p>
+          </div>
+          <div className="col-md-2 d-flex flex-column  align-items-center">
+            <div className="categorias-circle rounded-circle d-flex align-items-center justify-content-center my-md-3">
+              <span className="categorias-siglas h4 font-weight-bold mt-2">PMB</span>
+            </div>
+            <p className="text-sans-h5-bold text-center">Programa Mejoramiento de Barrios</p>
           </div>
         </div>
 
         <div className="container filter-line"></div>
 
         {/* Region select */}
-      <div className="mt-3">
+        <div className="mt-3">
         <div className="container d-flex justify-content-between align-items-start px-1">
-          <p>¿En qué región?</p>
+          <p className="text-sans-p">¿En qué región?</p>
           <button className="btn-limpiar" onClick={handleClearRegion}>
             Borrar <FontAwesomeIcon icon={faTrashCan} />
           </button>
@@ -91,9 +90,9 @@ const FiltroProyectos = () => {
       </select>
 
       {/* Comuna select */}
-      <p className="px-1">Elige una o más comunas</p>
+      <p className="text-sans-p px-1">¿En qué comuna?</p>
       <select className="container selectores text-underline text-muted">
-        <option value=''>Elige una opción</option>
+        <option value=''>Elige una o más comunas</option>
         {/* Map over the selectedComunas state to create options */}
         {selectedComunas.map((comuna, index) => (
           <option key={index} value={comuna.comuna}>
@@ -102,82 +101,82 @@ const FiltroProyectos = () => {
         ))}
       </select>
 
-        </div>
-
-        <div className="container filter-line my-3"></div>
-
-        <div>
-            <div className="container d-flex justify-content-between align-items-start px-1 mb-3">
-                <p>¿Qué tipo de proyecto es?</p>
-                <button className="btn-limpiar">
-                    Borrar <FontAwesomeIcon icon={faTrashCan} />
-                </button>
-            </div>
-
-            <div className="d-flex flex-wrap">
-                <div className="col-5 d-flex flex-column mx-2 align-items-center">
-                    <div className="categorias-circle rounded-circle d-flex align-items-center justify-content-center my-md-3">
-                        <img src="src/static/img/icon_categorias_landing.png" alt="Icono"/>
-                    </div>
-                    <p className="text-center font-weight-bold">Plazas y Áreas Verdes</p>
-                </div>
-                <div className="col-5 d-flex flex-column mx-2 align-items-center">
-                    <div className="categorias-circle rounded-circle d-flex align-items-center justify-content-center my-md-3">
-                        <img src="src/static/img/icon_categorias_landing.png" alt="Icono"/>
-                    </div>
-                    <p className="text-center font-weight-bold">Infraestructura Deportiva</p>
-                </div>
-                <div className="col-5 d-flex flex-column mx-2 align-items-center">
-                    <div className="categorias-circle rounded-circle d-flex align-items-center justify-content-center my-md-3">
-                        <img src="src/static/img/icon_categorias_landing.png" alt="Icono"/>
-                    </div>
-                    <p className="text-center font-weight-bold">Centros de Salud</p>
-                </div>
-                <div className="col-5 d-flex flex-column mx-2 align-items-center">
-                    <div className="categorias-circle rounded-circle d-flex align-items-center justify-content-center my-md-3">
-                        <img src="src/static/img/icon_categorias_landing.png" alt="Icono"/>
-                    </div>
-                    <p className="text-center font-weight-bold">Luminarias</p>
-                </div>
-                <div className="col-5 d-flex flex-column mx-2 align-items-center">
-                    <div className="categorias-circle rounded-circle d-flex align-items-center justify-content-center my-md-3">
-                        <img src="src/static/img/icon_categorias_landing.png" alt="Icono"/>
-                    </div>
-                    <p className="text-center font-weight-bold">Reciclaje</p>
-                </div>
-                <div className="col-5 d-flex flex-column mx-2 align-items-center">
-                    <div className="categorias-circle rounded-circle d-flex align-items-center justify-content-center my-md-3">
-                        <img src="src/static/img/icon_categorias_landing.png" alt="Icono"/>
-                    </div>
-                    <p className="text-center font-weight-bold">Energía</p>
-                </div>
-                <div className="col-5 d-flex flex-column mx-2 align-items-center">
-                    <div className="categorias-circle rounded-circle d-flex align-items-center justify-content-center my-md-3">
-                        <img src="src/static/img/icon_categorias_landing.png" alt="Icono"/>
-                    </div>
-                    <p className="text-center font-weight-bold">Desarrollo Técnológico</p>
-                </div>
-            </div>
-        </div>
-
-        <div className="container filter-line"></div>
-
-        <p className="mt-3 px-1">¿Qué años de construcción quieres ver?</p>
-        <div className="mb-4">
-            <input className="mx-2" type="checkbox" />
-            <label>2019</label> <br />
-            <input className="mx-2" type="checkbox" />
-            <label>2020</label> <br />
-            <input className="mx-2" type="checkbox" />
-            <label>2021</label>
-        </div>
-
-        <div className="d-flex justify-content-center">
-            <button className="btn-resultados mb-4">Mostrar resultados</button>
-        </div>
-
       </div>
-    );
-  };
 
-  export default FiltroProyectos;
+      <div className="container filter-line my-3"></div>
+
+      <div>
+        <div className="container d-flex justify-content-between align-items-start px-1 mb-3">
+          <p>¿Qué tipo de proyecto es?</p>
+          <button className="btn-limpiar">
+            Borrar <FontAwesomeIcon icon={faTrashCan} />
+          </button>
+        </div>
+
+        <div className="d-flex flex-wrap">
+          <div className="col-5 d-flex flex-column mx-2 align-items-center">
+            <div className="categorias-circle rounded-circle d-flex align-items-center justify-content-center my-md-3">
+              <img src="src/static/img/icon_categorias_landing.png" alt="Icono"/>
+            </div>
+            <p className="text-sans-h5-bold text-center">Plazas y Áreas Verdes</p>
+          </div>
+          <div className="col-5 d-flex flex-column mx-2 align-items-center">
+            <div className="categorias-circle rounded-circle d-flex align-items-center justify-content-center my-md-3">
+              <img src="src/static/img/icon_categorias_landing.png" alt="Icono"/>
+            </div>
+            <p className="text-sans-h5-bold text-center">Infraestructura Deportiva</p>
+          </div>
+          <div className="col-5 d-flex flex-column mx-2 align-items-center">
+            <div className="categorias-circle rounded-circle d-flex align-items-center justify-content-center my-md-3">
+              <img src="src/static/img/icon_categorias_landing.png" alt="Icono"/>
+            </div>
+            <p className="text-sans-h5-bold text-center">Centros de Salud</p>
+          </div>
+          <div className="col-5 d-flex flex-column mx-2 align-items-center">
+            <div className="categorias-circle rounded-circle d-flex align-items-center justify-content-center my-md-3">
+              <img src="src/static/img/icon_categorias_landing.png" alt="Icono"/>
+            </div>
+            <p className="text-sans-h5-bold text-center">Luminarias</p>
+          </div>
+          <div className="col-5 d-flex flex-column mx-2 align-items-center">
+            <div className="categorias-circle rounded-circle d-flex align-items-center justify-content-center my-md-3">
+              <img src="src/static/img/icon_categorias_landing.png" alt="Icono"/>
+            </div>
+            <p className="text-sans-h5-bold text-center">Reciclaje</p>
+          </div>
+          <div className="col-5 d-flex flex-column mx-2 align-items-center">
+            <div className="categorias-circle rounded-circle d-flex align-items-center justify-content-center my-md-3">
+              <img src="src/static/img/icon_categorias_landing.png" alt="Icono"/>
+            </div>
+            <p className="text-sans-h5-bold text-center">Energía</p>
+          </div>
+          <div className="col-5 d-flex flex-column mx-2 align-items-center">
+            <div className="categorias-circle rounded-circle d-flex align-items-center justify-content-center my-md-3">
+              <img src="src/static/img/icon_categorias_landing.png" alt="Icono"/>
+            </div>
+            <p className="text-sans-h5-bold text-center">Desarrollo Técnológico</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="container filter-line"></div>
+
+      <p className="text-sans-p mt-3 px-1">¿Qué años de construcción quieres ver?</p>
+      <div className="mb-4">
+        <input className="mx-2" type="checkbox" />
+        <label>2019</label> <br />
+        <input className="mx-2" type="checkbox" />
+        <label>2020</label> <br />
+        <input className="mx-2" type="checkbox" />
+        <label>2021</label>
+      </div>
+
+      <div className="d-flex justify-content-center">
+        <button className="btn-resultados mb-4">Mostrar resultados</button>
+      </div>
+
+    </div>
+  );
+};
+
+export default FiltroProyectos;
--- src/views/Landing/landing.jsx
+++ src/views/Landing/landing.jsx
@@ -1,5 +1,4 @@
 import BuscadorProyectos from "../../components/Commons/buscadorproyectos";
-import "../../static/styles/landing.css"
 
 const Home = () => {
   return (
@@ -8,28 +7,28 @@ const Home = () => {
 
       {/* Categorias principales */}
       <div className="container my-4 mb-md-5">
-        <p className="font-level-1 my-3 text-center">Categorías principales</p>
+        <p className="text-sans-h1 my-3 text-center">Categorías principales</p>
         <div className="container d-flex flex-row justify-content-center">
 
           <div className="col-md-2 d-flex flex-column mx-md-5 align-items-center">
             <div className="categorias-circle rounded-circle d-flex align-items-center justify-content-center my-md-3">
               <span className="categorias-siglas h4 font-weight-bold mt-2">PMU</span>
             </div>
-            <p className="text-body text-center">Programa Mejoramiento Urbano</p>
+            <p className="text-sans-p text-center">Programa Mejoramiento Urbano</p>
           </div>
           
           <div className="col-md-2 d-flex flex-column mx-md-5 align-items-center">
             <div className="categorias-circle rounded-circle d-flex align-items-center justify-content-center my-md-3">
               <span className="categorias-siglas h4 font-weight-bold mt-2">PMB</span>
             </div>
-            <p className="text-body text-center">Programa Mejoramiento de Barrios</p>
+            <p className="text-sans-p text-center">Programa Mejoramiento de Barrios</p>
           </div>
 
           <div className="col-md-2 d-flex flex-column mx-md-5 align-items-center">
             <div className="categorias-circle rounded-circle d-flex align-items-center justify-content-center my-md-3">
               <img src="src/static/img/icon_categorias_landing.png" alt="Icono"/>
             </div>
-            <p className="text-body text-center">Ver todos los proyectos</p>
+            <p className="text-sans-p text-center">Ver todos los proyectos</p>
           </div>
 
         </div>
@@ -38,12 +37,12 @@ const Home = () => {
 
       {/* Que es el Banco de Proyectos */}
       <div className="container col-md-8 px-4 mb-4">
-        <p className="font-level-1">¿Qué es el Banco de Proyectos?</p>
-        <p className="text-body "> El <strong> Banco de Proyectos </strong> de la Subsecretaría de Desarrollo Regional y Administrativo es un
+        <p className="text-sans-h2">¿Qué es el Banco de Proyectos?</p>
+        <p className="text-sans-p"> El <strong> Banco de Proyectos </strong> de la Subsecretaría de Desarrollo Regional y Administrativo es un
           <strong> repositorio de proyectos ya ejecutados por distintas municipalidades </strong>a lo largo del país, que tiene por objetivo
           <strong> poner a disposición diversas iniciativas y su documentación </strong>asociada al momento de postular a los
           <strong> Programas de Mejoramiento Urbano (PMU) y de Mejoramiento de Barrios (PMB) de esta Subsecretaría.</strong></p>
-        <p className="text-body my-md-4"> Toda la información en este repositorio es referencial, <strong>siendo responsabilidad de la unidad ejecutora revisar y actualizar 
+        <p className="text-sans-p my-md-4"> Toda la información en este repositorio es referencial, <strong>siendo responsabilidad de la unidad ejecutora revisar y actualizar 
           sus valores y aspectos normativos</strong>
         </p>
 
@@ -52,32 +51,32 @@ const Home = () => {
             <div className="que-es-circle rounded-circle d-flex align-items-center justify-content-center mb-md-4 my-4" >
               <img src="src/static/img/que_es_1.png" alt="Imagen" className="que-es-img img-fluid rounded-circle h-100 " />
             </div>
-            <strong className="text-center font-level-4 text-gray-a">Para personas encargadas de Programas de Mejoramiento Urbano y de Barrios</strong>
-            <p className="text-body text-center my-3 my-md-4">Dirigido a todo profesional municipal encargado de la formulación de proyectos que se enmarquen en los programas PMU y PMB.</p>
+            <strong className="text-sans-h4 text-center">Para personas encargadas de Programas de Mejoramiento Urbano y de Barrios</strong>
+            <p className="text-sans-p text-center my-3 my-md-4">Dirigido a todo profesional municipal encargado de la formulación de proyectos que se enmarquen en los programas PMU y PMB.</p>
           </div>
 
           <div className="col-md-4 d-flex flex-column mx-md-2 align-items-center">
             <div className="que-es-circle rounded-circle d-flex align-items-center justify-content-center mb-md-4 my-4" >
               <img src="src/static/img/que_es_2.png" alt="Imagen" className="que-es-img img-fluid rounded-circle h-100" />
             </div>
-            <strong className="text-center font-level-4 text-gray-a">Filtra información y encuentra el proyecto que se ajuste a tu comuna</strong>
-            <p className="text-body text-center my-3 my-md-4">Podrás filtrar proyectos por tipo de programa, revisar antecedentes y descargar documentación referencial para postular proyectos adecuados a la realidad de tu comuna.</p>
+            <strong className="text-sans-h4 text-center">Filtra información y encuentra el proyecto que se ajuste a tu comuna</strong>
+            <p className="text-sans-p text-center my-3 my-md-4">Podrás filtrar proyectos por tipo de programa, revisar antecedentes y descargar documentación referencial para postular proyectos adecuados a la realidad de tu comuna.</p>
           </div>
 
           <div className="col-md-4 d-flex flex-column mx-md-2 align-items-center">
             <div className="que-es-circle rounded-circle d-flex align-items-center justify-content-center mb-md-4 my-4" >
               <img src="src/static/img/que_es_3.png" alt="Imagen" className="que-es-img img-fluid rounded-circle" />
             </div>
-            <strong className="text-center font-level-4 text-gray-a">Entregamos información referencial para la realización de proyectos</strong>
-            <p className="text-body text-center my-3 my-md-4">Encontrarás información referencial detallada de proyectos como: planos, especificaciones, presupuestos, e información referencial para su elaboración. </p>
+            <strong className="text-sans-h4 text-center">Entregamos información referencial para la realización de proyectos</strong>
+            <p className="text-sans-p text-center my-3 my-md-4">Encontrarás información referencial detallada de proyectos como: planos, especificaciones, presupuestos, e información referencial para su elaboración. </p>
           </div>
         </div>
       </div>
 
       {/* Quienes somos */}
       <div className="container col-md-8 px-4">
-        <p className="font-level-1 text-center">¿Quiénes somos?</p>
-        <p className="text-body">El Banco de Proyectos es una iniciativa directa de la  Subsecretaría de Desarrollo Regional y Administrativo, cuya información está en permanente actualización.</p>
+        <p className="text-sans-h2 text-center">¿Quiénes somos?</p>
+        <p className="text-sans-p">El Banco de Proyectos es una iniciativa directa de la  Subsecretaría de Desarrollo Regional y Administrativo, cuya información está en permanente actualización.</p>
       </div>
 
       {/* Banner mapa chile */}
