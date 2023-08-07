b30dd177e23573450d3d1cee841c588d5730bb35
Rosario Herrera Amunátegui <114428069+RosarioHA@users.noreply.github.com>
2023-08-07 16:09:26 -0400

    agrega divisor para footer disp mobile

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -2,13 +2,11 @@ import { Link } from 'react-router-dom';
 
 const Footer = () => {
     return (
-      <footer className="footer-container content-always-on-display">
+      <footer className="footer-container content-always-on-display position-relative">
         <div className="container py-5">
 
         {/* linea decorativa?? */}
-        <div className="line w-50 d-sm-none"></div>
-        <div className="line d-none d-sm-block"></div>
-        <div className="deco-line">
+        <div className="deco-line position-absolute">
           <div className="azul"></div>
           <div className="rojo"></div>
         </div>
@@ -21,13 +19,17 @@ const Footer = () => {
               </div>  
             </div>
 
-            <div className="col-md-3 a11y-fonts-col-12">
+            <div className="col-md-3 ms-3 ms-md-0 d-flex flex-column">
               <p className="text-sans-p-white mb-3">Secciones</p>
               <Link to="/" className="text-sans-p-white text-decoration-underline"> Inicio </Link>
               <Link to="/bancodeproyectos" className="text-sans-p-white text-decoration-underline"> Banco de Proyectos </Link>
             </div>
 
-            <div className="col-md-3 a11y-fonts-col-12 h-100 mt-5 mt-md-0">
+            <div className="d-lg-none d-flex justify-content-center">
+              <div className="divider col-11 mt-4"></div>
+            </div>
+
+            <div className="col-md-3 d-flex flex-column h-100 mt-5 mt-md-0 ms-3 ms-md-0">
               <p className="text-sans-p-white mb-3">Enlaces de Interés</p>
               <a className="text-sans-p-white text-decoration-underline" href="https://www.subdere.gov.cl/programas/divisi%C3%B3n-municipalidades/programa-mejoramiento-urbano-y-equipamiento-comunal-pmu" target="_blank" rel="noreferrer">Programa Mejoramiento Urbano</a>
               <a className="text-sans-p-white text-decoration-underline" href="https://www.subdere.gov.cl/programas/divisi%C3%B3n-municipalidades/programa-mejoramiento-de-barrios-pmb" target="_blank" rel="noreferrer">Programa Mejoramiento de Barrios</a>
@@ -35,7 +37,11 @@ const Footer = () => {
               <a className="text-sans-p-white text-wrap h-75 text-underline" href="https://www.interior.gob.cl/" target="_blank" rel="noreferrer">Ministerio del Interior y Seguridad Pública</a>
             </div>
 
-            <div className="col-md-3 a11y-fonts-col-12 mt-5 mt-md-0">
+            <div className="d-lg-none d-flex justify-content-center">
+              <div className="divider col-11 mt-4"></div>
+            </div>
+
+            <div className="col-md-3 d-flex flex-column mt-5 mt-md-0 ms-3 ms-md-0">
               <p className="text-sans-p-white mb-3">Ayuda</p>
               <Link to="/contacto" className="text-sans-p-white text-decoration-underline">Ingresa un Formulario de Contacto</Link>
               <p className="text-sans-p-white mb-3 mt-md-5">Dirección:</p>
