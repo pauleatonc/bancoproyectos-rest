ab279c1f1890150e2b7080b28f33062deae65769
Rosario Herrera Amunátegui <114428069+RosarioHA@users.noreply.github.com>
2023-08-09 11:37:57 -0400

    elimina pruebas fallidas vista bancodeproyectos

--- src/views/Projects/bancodeproyectos/bancodeproyectos.jsx
+++ src/views/Projects/bancodeproyectos/bancodeproyectos.jsx
@@ -4,35 +4,6 @@ import FiltroProyectos from "../../../components/Bancodeproyectos/proyectosFilte
 import SortProyectos from "../../../components/Bancodeproyectos/proyectosSort";
 import ProyectosContainer from "../../../components/Bancodeproyectos/proyectosContainer";
 
-// const BancoProyectos = () => {
-//     return (
-//       <div className="container col-md-10">
-
-//         <nav className="container" aria-label="breadcrumb">
-//           <ol className="breadcrumb breadcrumb-style">
-//             <li className="breadcrumb-item"><a href="/" >Inicio</a></li>
-//             <li className="breadcrumb-item active" aria-current="page">Banco de Proyectos</li>
-//           </ol>
-//         </nav>
-
-//         <BuscadorProyectos />
-        
-//         <div className="container row">
-//           <div className="col-lg-4">
-//             <FiltroProyectos />
-//           </div>
-//           <div className="col-lg-8 d-flex flex-column align-items-end">
-//             <SortProyectos />
-//             <ProyectosContainer />
-//           </div>
-
-//         </div>
-        
-//       </div>
-//     );
-//   };
-//   export default BancoProyectos;
-
   const BancoProyectos = () => {
     return (
       <div className="container col-md-10">
