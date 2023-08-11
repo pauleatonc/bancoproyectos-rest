2230828efd9a775d8b26afc930390ecbd6b93c66
Rosario Herrera Amunátegui <114428069+RosarioHA@users.noreply.github.com>
2023-08-11 14:51:10 -0400

    agrega logica para contar miniaturas ocultas

--- src/components/Proyecto/carrusel.jsx
+++ src/components/Proyecto/carrusel.jsx
@@ -1,39 +1,78 @@
-const Carrusel = () => {
-    return (
-      <div className="container text-center">
-        {/* Imagen portada */}
-        <div className="row d-none d-lg-block img-portada my-4">
-          <div className="col d-flex justify-content-center">
-            <div className="img-proyecto col-lg-7">Portada</div>
-          </div>
-        </div>
+import { useState, useEffect, useRef } from 'react';
 
-        {/* Miniaturas */}
-        <div className="container mini-container d-flex flex-wrap justify-content-center">
-            <div className="miniatura m-1">
-                <div>img</div>
-            </div>
-            <div className="miniatura m-1">
-                <div>img</div>
-            </div>
-            <div className="miniatura m-1">
-                <div>img</div>
-            </div>
-            <div className="miniatura m-1">
-                <div>img</div>
-            </div>
-            <div className="miniatura m-1">
-                <div>img</div>
-            </div>
-            <div className="miniatura m-1">
-                <div>img</div>
-            </div>
-            <div className="miniatura m-1">
-                <div>img</div>
-            </div>
-        </div>    
+const Carrusel = () => {
+  const [hiddenThumbnails, setHiddenThumbnails] = useState(0);
+  const miniContainerRef = useRef(null);
+
+  useEffect(() => {
+    const miniContainer = miniContainerRef.current;
+
+    const updateHiddenThumbnails = () => {
+      if (miniContainer) {
+        const miniaturaStyles = window.getComputedStyle(miniContainer.firstElementChild);
+        const miniaturaWidth = miniContainer.firstElementChild.offsetWidth + parseFloat(miniaturaStyles.marginLeft) + parseFloat(miniaturaStyles.marginRight);
+        const containerWidth = miniContainer.offsetWidth;
+        const visibleThumbnailsCount = Math.floor(containerWidth / miniaturaWidth);
+        const totalThumbnailsCount = miniContainer.children.length;
+        const hiddenThumbnailsCount = totalThumbnailsCount - visibleThumbnailsCount;
+        setHiddenThumbnails(hiddenThumbnailsCount);
+      }
+    };
+
+    updateHiddenThumbnails();
+    window.addEventListener('resize', updateHiddenThumbnails);
+
+    return () => {
+      window.removeEventListener('resize', updateHiddenThumbnails);
+    };
+  }, []);
+  
+  return (
+    <div className="container text-center">
+      {/* Imagen portada */}
+      <div className="row d-none d-lg-block img-portada my-4">
+        <div className="col d-flex justify-content-center">
+          <div className="img-proyecto col-lg-7">Portada</div>
+        </div>
       </div>
-    );
-  };
+
+      {/* Miniaturas */}
+      <div className="container mini-container d-flex flex-wrap justify-content-center" ref={miniContainerRef}>
+        <div className="miniatura m-1">
+          <div>img</div>
+        </div>
+        <div className="miniatura m-1">
+          <div>img</div>
+        </div>
+        <div className="miniatura m-1">
+          <div>img</div>
+        </div>
+        <div className="miniatura m-1">
+          <div>img</div>
+        </div>
+        <div className="miniatura m-1">
+          <div>img</div>
+        </div>
+        <div className="miniatura m-1">
+          <div>img</div>
+        </div>
+        <div className="miniatura m-1">
+          <div>img</div>
+        </div>
+        <div className="miniatura m-1">
+          <div>img</div>
+        </div>
+      </div>
+
+      {hiddenThumbnails > 0 && (
+        <p>{hiddenThumbnails} miniaturas ocultas</p>
+      )}
+
+    </div>
+
     
-  export default Carrusel;
+
+  );
+};
+    
+export default Carrusel;
