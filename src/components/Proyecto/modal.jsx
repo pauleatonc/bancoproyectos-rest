5c0aa5ed8865db3f43617f09a836696955f56510
Rosario Herrera Amunátegui <114428069+RosarioHA@users.noreply.github.com>
2023-08-19 19:05:17 -0400

    soluciona problemas indice de imagen

--- src/components/Proyecto/modal.jsx
+++ src/components/Proyecto/modal.jsx
@@ -1,28 +1,39 @@
+import { useState, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
-const ImageModal = ({img, selectedImageIndex, setSelectedImageIndex}) => {
-  const handleModalClose = () => {
-    setSelectedImageIndex(0); // Reinicia el indice al cerrar el modal
-  }
+const ImageModal = ({ img, selectedImageIndex, setSelectedImageIndex }) => {
+  const [currentImageIndex, setCurrentImageIndex] = useState(selectedImageIndex);
 
-  const imageIndicator = `${selectedImageIndex + 1} de ${img.length}`;
+  const handleCarouselSlide = newIndex => {
+    setCurrentImageIndex(newIndex);
+  };
+
+  const imageIndicator = `${currentImageIndex + 1} de ${img.length}`;
+
+  useEffect(() => {
+    setCurrentImageIndex(selectedImageIndex); // Sincroniza currentImageIndex cuando selectedImageIndex cambia
+  }, [selectedImageIndex]);
+
+  const handleClose = () => {
+    setSelectedImageIndex(currentImageIndex); // Restaura selectedImageIndex al valor de currentImageIndex
+  };
 
   return (
     <div className="modal fade" id="imageModal" tabIndex="-1" aria-labelledby="imageModalLabel" aria-hidden="true">
       <div className="modal-dialog modal-lg modal-dialog-centered">
         <div className="modal-content border border-secondary">
           <div className="d-flex justify-content-end">
-            <button type="button" className=" custom-close-button" data-bs-dismiss="modal" aria-label="Close" onClick={handleModalClose}>
+            <button type="button" className=" custom-close-button" data-bs-dismiss="modal" aria-label="Close" onClick={handleClose}>
               Cerrar <FontAwesomeIcon icon={faTimes}/>
             </button>
           </div>
           
           <div className="modal-body">
-            <div id="carouselExampleControls" className="carousel slide"  key={selectedImageIndex}>
+            <div id="carouselExampleControls" className="carousel slide"  key={currentImageIndex}>
             <div className="carousel-inner">
               {img.map((image, index) => (
-                <div className={`carousel-item ${index === selectedImageIndex ? 'active' : ''}`} key={index}>
+                <div className={`carousel-item ${index === currentImageIndex ? 'active' : ''}`} key={index}>
                   <img className="d-block w-100" src={image} alt={`Image ${index}`} />
                 </div>
               ))}
@@ -30,11 +41,11 @@ const ImageModal = ({img, selectedImageIndex, setSelectedImageIndex}) => {
 
             <div className="image-indicator">{imageIndicator}</div>
 
-              <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+              <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev" onClick={() => handleCarouselSlide((currentImageIndex - 1 + img.length) % img.length)}>
                 <span className="carousel-btn carousel-control-prev-icon" aria-hidden="true"></span>
                 <span className="visually-hidden">Previous</span>
               </button>
-              <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+              <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next" onClick={() => handleCarouselSlide((currentImageIndex + 1) % img.length)}>
                 <span className="carousel-btn carousel-control-next-icon" aria-hidden="true"></span>
                 <span className="visually-hidden">Next</span>
               </button>
