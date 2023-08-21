91633335ccf246643ad99ab38e9019d79dcb21e7
Rosario Herrera Amunátegui <114428069+RosarioHA@users.noreply.github.com>
2023-08-21 17:44:53 -0400

    abre modulo al hacer click en portada. Agrega hover a portada y miniaturas

--- src/components/Proyecto/carrusel.jsx
+++ src/components/Proyecto/carrusel.jsx
@@ -31,14 +31,14 @@ const Carrusel = ({ imgPortada, imgGeneral }) => {
 
   return (
     <div className="container text-center mb-md-5 d-flex flex-column align-items-center">
+      {/* Portada */}
       <div className="col-10 d-none d-md-block img-portada my-4 d-flex justify-content-center">
+      <a data-bs-toggle="modal" data-bs-target="#imageModal" onClick={() => setSelectedImageIndex(0)}>
         <img className="img-fluid" src={imgPortada} />
+      </a>
       </div>
 
-      <div className="thumbnail-counter border border-primary p-4">
-        +{hiddenThumbnailsCount}
-      </div>
-
+      {/* Miniaturas */}
       <div className="container-fluid container-md mini-container d-flex flex-wrap justify-content-center" ref={miniContainerRef}>
         {imgArray.map((image, index) => (
           <div className={`m-1 m-md-2 miniatura`} key={index}>
