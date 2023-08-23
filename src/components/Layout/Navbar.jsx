ac7b8d4bd3040113e6a104ab6e936b0eb36f3e33
Rosario Herrera Amunátegui <114428069+RosarioHA@users.noreply.github.com>
2023-08-23 13:22:27 -0400

    crea menu desplegable en vista mobile, elimina archivo navbarAntiguo.jsx

--- src/components/Layout/Navbar.jsx
+++ src/components/Layout/Navbar.jsx
@@ -1,6 +1,28 @@
 import { Link } from 'react-router-dom';
+import { useState, useRef, useEffect } from 'react';
 
 const Navbar = () =>{  
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const menuRef = useRef(null); // Referencia al menu desplegable
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const handleOutsideClick = (event) => {
+    if (menuRef.current && !menuRef.current.contains(event.target)){
+      setIsMenuOpen(false); //Cierra menu al hacer click fuera de el
+    }
+  }
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleOutsideClick);
+
+    return () => {
+      document.removeEventListener('mousedown', handleOutsideClick);
+    };
+  }, []);
+
   return (
   <>
     <div id="barraGris"></div>
@@ -20,10 +42,11 @@ const Navbar = () =>{
 
         <div className="col d-flex justify-content-end  border border-secondary">
           {/* Inicio sesion */}
-          <button className="btn-principal-s mt-4 me-md-5" type="button">
+          <button className="btn-principal-s mt-4 me-md-5 d-none d-md-block" type="button">
             <i className="cl cl-claveunica"/> 
-            <span className="text-underline mx-1">Iniciar sesión</span>
+            <span className="text-underline mx-1"> Iniciar sesión </span>
           </button>
+          <button className="d-md-none" onClick={toggleMenu}> Menu </button>
         </div>
       </div>
     </nav>  
@@ -31,13 +54,34 @@ const Navbar = () =>{
     <div className="row border border-primary">
       {/* Navegacion */}
       <div className="mx-md-auto mx-lg-auto mx-xl-auto d-flex flex-column flex-md-row justify-content-center">
+        <Link to="/" className="link text-black text-underline mx-md-3 d-none d-md-block"> Inicio </Link>
+        <Link to="/bancodeproyectos" className="link text-black text-underline mx-md-3 d-none d-md-block"> Banco de Proyectos </Link>
+        <Link to="/bancodeproyectos" className="link text-black text-underline mx-md-3 d-none d-md-block"> Type something </Link>
+        <Link to="/bancodeproyectos" className="link text-black text-underline mx-md-3 d-none d-md-block"> Type something </Link>
+        <Link to="/contacto" className="link text-black text-underline mx-md-3 d-none d-md-block">Contacto</Link>
+      </div>
+    </div> 
+
+    {/* Menu Desplegable */}
+    {isMenuOpen && (
+      <div className="col" ref={menuRef}>
+        {/* nagevacion */}
         <Link to="/" className="link text-black text-underline mx-md-3"> Inicio </Link>
         <Link to="/bancodeproyectos" className="link text-black text-underline mx-md-3"> Banco de Proyectos </Link>
         <Link to="/bancodeproyectos" className="link text-black text-underline mx-md-3"> Type something </Link>
         <Link to="/bancodeproyectos" className="link text-black text-underline mx-md-3"> Type something </Link>
         <Link to="/contacto" className="link text-black text-underline mx-md-3">Contacto</Link>
+
+        <hr />
+        {/* Inicio Sesion  */}
+        <button className="btn-principal-l mt-4 me-md-5" type="button">
+            <i className="cl cl-claveunica"/> 
+            <span className="text-underline mx-1"> Iniciar sesión </span>
+          </button>
       </div>
-    </div>   
+    )}
+
+
   </>
 );
 };
