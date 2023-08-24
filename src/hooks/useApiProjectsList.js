0f61a42338ff315d8cd306e9e5128c876c954d52
JaimeHernandezE <jhearquitecto@gmail.com>
2023-08-23 20:10:19 -0400

    FIX: Api filter mejorada

--- src/hooks/useApiProjectsList.js
+++ src/hooks/useApiProjectsList.js
@@ -2,25 +2,27 @@ import { useState, useEffect } from 'react';
 import apiProjects from '../services/project/projects.api';
 
 const useApiProjectsList = () => {
-  const [ dataProject, setDataProject ] = useState([]);
-  const [ loadingProject, setLoadingProject ] = useState(true);
-  const [ errorProject, setErrorProject ] = useState(null);
+  const [dataProject, setDataProject] = useState([]);
+  const [loadingProject, setLoadingProject] = useState(true);
+  const [errorProject, setErrorProject] = useState(null);
 
-  useEffect( ()=>{
-    const fetchData = async () => {
-      try {
-        const response = await apiProjects.get('v1/');
-        setDataProject(response.data);
-        setLoadingProject(false);
-      } catch (error) {
-        setErrorProject(error);
-        setLoadingProject(false);
-      }
-    };
-    fetchData();
+  const fetchProjects = async (endpoint = 'v1/') => {
+    try {
+      setLoadingProject(true);
+      const response = await apiProjects.get(endpoint);
+      setDataProject(response.data);
+      setLoadingProject(false);
+    } catch (error) {
+      setErrorProject(error);
+      setLoadingProject(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchProjects();
   }, []);
 
-  return { dataProject, loadingProject, errorProject};
+  return { dataProject, loadingProject, errorProject, fetchProjects };
 }
 
 export default useApiProjectsList;
--- src/views/Projects/bancodeproyectos/bancodeproyectos.jsx
+++ src/views/Projects/bancodeproyectos/bancodeproyectos.jsx
@@ -1,69 +1,61 @@
-import React, { useState } from 'react';
+import { useState, useMemo } from 'react';
+import { Link } from 'react-router-dom';
 import "../../../static/styles/bancodeproyectos.css";
-import {ProyectoContainer , ProyectosFilter, ProyectosSort , BuscadorProyectos} from '../../../components/Bancodeproyectos';
+import { ProyectoContainer, ProyectosFilter, ProyectosSort, BuscadorProyectos } from '../../../components/Bancodeproyectos';
 import useApiFilter from '../../../hooks/useApiFilter';
 import useApiProjectsList from "../../../hooks/useApiProjectsList";
 
+const BancoProyectos = () => {
+  const { dataProject, loadingProject, errorProject } = useApiProjectsList();
+  const { dataFilter, loading, error, filteredProjects, hasResults, handleFilter } = useApiFilter();
 
 
-  const BancoProyectos = () => {
-    const { dataProject, loadingProject, errorProject } = useApiProjectsList();
-    const { loading, error, filteredProjects, hasResults, handleFilter } = useApiFilter();
+  const [searchActivated, setSearchActivated] = useState(false);
+  const [searchResults, setSearchResults] = useState([]);
 
-     // State to hold search results
-    const [searchActivated, setSearchActivated] = useState(false);
-    const [searchResults, setSearchResults] = useState([]);
-    
-    // Handler to update the state with search results
-    const handleSearch = (results) => {
-      setSearchResults(results);
-      setSearchActivated(true);
-    }
 
-    let projectsToDisplay = [];
-
-    if (searchActivated) {
-      projectsToDisplay = searchResults;
-    } else {
-      projectsToDisplay = filteredProjects.length > 0 ? filteredProjects : dataProject;
-    }
-
-    if (loadingProject || loading) {
-      return <div>CARGANDO DATOS...</div>
-    }
-    if (errorProject || error) {
-      return <div>Error de conexion</div>
-    }
-    
-    return (
-      <div className="container col-md-10">
-
-        <nav className="container" aria-label="breadcrumb">
-          <ol className="breadcrumb breadcrumb-style">
-            <li className="breadcrumb-item"><a href="/" >Inicio</a></li>
-            <li className="breadcrumb-item active" aria-current="page">Banco de Proyectos</li>
-          </ol>
-        </nav>
-
-        <BuscadorProyectos onSearch={handleSearch}/>
-        
-        <div className="container d-flex flex-column flex-md-row">
-          <ProyectosFilter onFilter={handleFilter}/>
-          <div className="ml-md-5">
-            <div className="d-flex justify-content-end mb-1">
-              <ProyectosSort/>
-            </div>
-            {hasResults ? (
-                  <ProyectoContainer data={projectsToDisplay} />
-              ) : (
-                  <div className="d-flex justify-content-center mt-4">
-                      <p className="text-muted">No hay proyectos que coincidan con los criterios seleccionados.</p>
-                  </div>
-              )}
-          </div>
-        </div>
-        
-      </div>
-    );
+  const handleSearch = (results) => {
+    setSearchResults(results);
+    setSearchActivated(true);
   };
-  export default BancoProyectos;
+
+  
+  const projectsToDisplay = useMemo(() => {
+    return searchActivated ? searchResults : filteredProjects.length > 0 ? filteredProjects : dataProject;
+  }, [searchActivated, searchResults, filteredProjects, dataProject]);
+  
+  if (loadingProject || loading) return <div>CARGANDO DATOS...</div>;
+  if (errorProject || error) return <div>Error de conexión</div>;
+
+
+  return (
+    <div className="container col-md-10">
+      <nav className="container" aria-label="breadcrumb">
+        <ol className="breadcrumb breadcrumb-style">
+          <li className="breadcrumb-item"><Link to="/">Inicio</Link></li>
+          <li className="breadcrumb-item active" aria-current="page">Banco de Proyectos</li>
+        </ol>
+      </nav>
+
+      <BuscadorProyectos onSearch={handleSearch} />
+
+      <div className="container d-flex flex-column flex-md-row">
+        <ProyectosFilter onFilter={handleFilter} dataFilter={dataFilter}/>
+        <div className="ml-md-5">
+          <div className="d-flex justify-content-end mb-1">
+            <ProyectosSort />
+          </div>
+          {hasResults ? (
+            <ProyectoContainer data={projectsToDisplay} />
+          ) : (
+            <div className="d-flex justify-content-center mt-4">
+              <p className="text-muted">No hay proyectos que coincidan con los criterios seleccionados.</p>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BancoProyectos;
