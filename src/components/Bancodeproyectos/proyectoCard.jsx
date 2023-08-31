429dcfd98c4f0747c30a2df94520d0bb80f61a6b
Rosario Herrera Amunátegui <114428069+RosarioHA@users.noreply.github.com>
2023-08-31 13:52:24 -0400

    ajusta distribucion tarjetas de proyectos

--- src/components/Bancodeproyectos/proyectoCard.jsx
+++ src/components/Bancodeproyectos/proyectoCard.jsx
@@ -28,27 +28,32 @@ const ProyectoCard = ({ project = {} }) => {
   const truncatedDescription = truncateText(description, 20);
 
   return (
-    <div className="proyect-card m-3 col-6 ">
-      <div className="img-container d-flex justify-content-center">
-        <img src={portada} className="image p-1" alt={name} />
+    <div className="proyect-card m-3 col-6 d-flex flex-column justify-content-between">
+      <div>
+        <div className="img-container d-flex justify-content-center">
+          <img src={portada} className="image p-1" alt={name} />
+        </div>
+        <div className="d-flex flex-row justify-content-between ms-3 my-2">
+          <p className="col-6 text-sans-h5 text-muted">Región: {region}</p>
+          <p className="col-5 text-sans-h5 text-muted">Comuna: {comunaName}</p>
+        </div>
+        <h2 className="text-serif-h2 text-decoration-underline ml-3 mb-3 mx-3">{name}</h2>
+        <p className="text-sans-p mx-3">{truncatedDescription}</p>
       </div>
-      <div className="d-flex flex-row justify-content-between ms-3 my-2">
-        <p className="col-6 text-sans-h5 text-muted">Región: {region}</p>
-        <p className="col-5 text-sans-h5 text-muted">Comuna: {comunaName}</p>
-      </div>
-      <h2 className="text-serif-h2 text-decoration-underline ml-3 mb-3 mx-3">{name}</h2>
-      <p className="text-sans-p mx-3">{truncatedDescription}</p>
-      <div className="container d-flex justify-content-between">
-        <p className="tag p-1">{program.sigla}</p>
-        <p className="tag p-1">{type.name}</p>
-        <p className="tag p-1">{year.number}</p>
-      </div>
-      <div className="d-flex justify-content-end p-3">
-        <button className="font-level-4 btn-principal-s text-decoration-underline px-3">
-          <Link to={`/project/${slug}/`} className="font-level-4 btn-principal-s text-decoration-underline px-3">
-            Ver más &gt;
-          </Link>
-        </button>
+
+      <div>
+        <div className="container d-flex justify-content-between">
+          <p className="tag p-1">{program.sigla}</p>
+          <p className="tag p-1">{type.name}</p>
+          <p className="tag p-1">{year.number}</p>
+        </div>
+        <div className="d-flex justify-content-end p-3">
+          <button className="font-level-4 btn-principal-s text-decoration-underline px-3">
+            <Link to={`/project/${slug}/`} className="font-level-4 btn-principal-s text-decoration-underline px-3">
+              Ver más &gt;
+            </Link>
+          </button>
+        </div>
       </div>
     </div>
   );
