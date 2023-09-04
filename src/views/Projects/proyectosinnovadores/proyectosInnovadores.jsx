17798a91b8e81e1f354adec186996ddafce3ae43
Rosario Herrera Amunátegui <114428069+RosarioHA@users.noreply.github.com>
2023-09-04 14:27:33 -0300

    crea elementos y estructura vista proyectos innovadores

--- src/views/Projects/proyectosinnovadores/proyectosInnovadores.jsx
+++ src/views/Projects/proyectosinnovadores/proyectosInnovadores.jsx
@@ -1,6 +1,52 @@
 const ProyectosInnovadores = () => {
   return (
-    <div> Proyectos Innovadores</div>
+    <div className="container">
+      <p>Breadcrumbs</p>
+      <h1>Proyectos Innovadores</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+      </p>
+      <h2>Listado de proyectos innovadores</h2>
+      <h3>Primero, elige un programa:</h3>
+
+      <div className="border border-alert">
+        botones PMU PMB
+      </div>
+
+      <p>Los espacios públicos, al igual que nuestra sociedad, son dinámicos y varían acorde a los tiempos y lugares en los que se encuentran. Es por esto, que la innovación en el espacio urbano se hace fundamental a la hora de entregar a la ciudadanía una mejor, más amplia y moderna oferta de espacio público.
+        Aquí te mostramos algunas ideas de espacios deportivos, culturales y de protección ambiental para que puedas considerar posibles soluciones a desarrollar con financiamiento PMU.
+      </p>
+
+      <div className="border border-alert">
+        botones proyectos innovadores - dropdown
+      </div>
+
+      <div>
+        <h4>Nombre Proyecto</h4>
+        <p>Los Patinódromos son pistas de asfalto que permiten el deslizamiento de patines, fomentan la competencia sana y la vida saludable.
+          Es una infraestructura que no requiere mucha mantención y ofrece una disciplina diferente a los deportes tradicionales como fútbol, basquetbol y tenis.
+        </p>
+        <div className="border border-alert"> Carrusel 1</div>
+        <div className="d-flex flex-column">
+          <a>Visitar fuente 1 </a>
+          <a>Visitar fuente 1 </a>
+        </div>
+      </div>
+
+      <hr />
+
+      <h2>Buenas prácticas para el diseño de los espacios públicos</h2>
+      <p>Con estas prácticas buscamos promover criterios sustentables a considerar en el diseño actual de los espacios públicos.</p>
+      <div className="row">
+        <div className="col">
+          menu con buenas practicas
+        </div>
+        <div className="col">
+          detalle practica seleccionada
+          <div className="border border-alert"> Carrusel 2</div>
+        </div>
+      </div>
+    </div>
   );
 };
 
