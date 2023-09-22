6281580d9d07fdde5e50730fcaf6dc86b0ae8223
vecarope <vecarope@mac-vcaro.local>
2023-09-22 11:32:05 -0300

    add yup en formulario contacto

--- src/components/Contacto/contactoForm.jsx
+++ src/components/Contacto/contactoForm.jsx
@@ -1,29 +1,13 @@
 import useContactForm from '../../hooks/useContactForm';
-import {apiBancoProyecto } from '../../services/bancoproyecto.api.js';
+
 
 const ContactForm = () => {
-
-  const submitForm = async (formData) => {
-    try {
-        const response = await apiBancoProyecto.post('contact/v1/', formData);
-        if (response.status === 200) {
-            console.log('Form data submitted successfully:', response.data);
-            // You can add additional logic here, e.g., showing a success message, redirecting, etc.
-        } else {
-            console.log('Failed to submit form data:', response.data);
-        }
-    } catch (error) {
-        console.error('Error submitting form data:', error);
-        // Handle errors appropriately, e.g., showing an error message to the user.
-    }
-  };
-
   const {
-      formData,
-      formErrors,
-      handleChange,
-      handleSubmit
-  } = useContactForm(submitForm);
+    register,
+    handleSubmit,
+    errors
+  } = useContactForm();
+
 
   return (
     <div className="d-flex justify-content-center mt-4 mb-5">
@@ -32,84 +16,74 @@ const ContactForm = () => {
         <div className="d-flex flex-column input-container">
           <label className="text-sans-p input-label ms-3 ms-sm-0" htmlFor="full_name">Nombre Completo (Obligatorio)</label>
           <input
+            {...register('full_name')}
             className="input-s px-3"
             type="text"
             id="full_name"
-            name="full_name"
             placeholder="Ingresa tu nombre."
-            value={formData.full_name}
-            onChange={handleChange}
           />
-          {formErrors.full_nameError && <span>{formErrors.full_nameError}</span>}
+          {errors.full_name && <span>{errors.full_name.message}</span>}
         </div>
 
         <div className="d-flex flex-column input-container mt-4">
           <label className="text-sans-p input-label ms-3 ms-sm-0" htmlFor="email">Correo electrónico institucional (Obligatorio)</label>
           <input
+            {...register('email')}
             className="input-s px-3"
             type="email"
             id="email"
-            name="email"
             placeholder="Ingresa tu correo electrónico."
-            value={formData.email}
-            onChange={handleChange}
           />
-          {formErrors.emailError && <span>{formErrors.emailError}</span>}
+          {errors.email && <span>{errors.email.message}</span>}
         </div>
 
         <div className="d-flex flex-column input-container mt-4">
           <label className="text-sans-p input-label ms-3 ms-sm-0" htmlFor="organization">Organización a la que perteneces (Obligatorio)</label>
           <input
+            {...register('organization')}
             className="input-s px-3"
             type="text"
             id="organization"
-            name="organization"
             placeholder="Ingresa el nombre de tu organización."
-            value={formData.organization}
-            onChange={handleChange}
           />
-          {formErrors.organizationError && <span>{formErrors.organizationError}</span>}
+          {errors.organization && <span>{errors.organization.message}</span>}
         </div>
 
         <div className="d-flex flex-column mt-2">
           <label className="text-sans-p px-3" htmlFor="contact_reason">Razón de contacto (Obligatorio)</label>
           <select
-              className="custom-select px-3"
-              id="contact_reason"
-              name="contact_reason"
-              value={formData.contact_reason}
-              onChange={handleChange}>
-              <option value="">Elije una opción</option>
-              <option value="sugerencia">Sugerencia</option>
-              <option value="consulta">Consulta por programa</option>
-              <option value="documento">Falta un documento</option>
-              <option value="falla">Falla en la plataforma</option>
+            {...register('contact_reason')}
+            className="custom-select px-3"
+            id="contact_reason">
+            <option value="">Elije una opción</option>
+            <option value="sugerencia">Sugerencia</option>
+            <option value="consulta">Consulta por programa</option>
+            <option value="documento">Falta un documento</option>
+            <option value="falla">Falla en la plataforma</option>
           </select>
-          {formErrors.contact_reasonError && <span>{formErrors.contact_reasonError}</span>}
+          {errors.contact_reason && <span>{errors.contact_reason.message}</span>}
         </div>
 
         <div className="d-flex flex-column input-container mt-4">
           <label className="text-sans-p input-label ms-3 ms-sm-0" htmlFor="message">Comentario (Obligatorio)</label>
           <textarea
+            {...register('message')}
             className="input-l p-3"
             id="message"
-            name="message"
-            value={formData.message}
-            onChange={handleChange}
             placeholder="Describe la razón de contacto."
           ></textarea>
-          {formErrors.messageError && <span>{formErrors.messageError}</span>}
+          {errors.message && <span>{errors.message.message}</span>}
         </div>
 
         <div className="text-sans-h5 text-end opacity-50 mt-1">
-          {formData.message.length} / 250 palabras
+          {/* Puedes utilizar watch para obtener el valor en tiempo real del textarea */}
+          {/* {watch('message').length} / 250 palabras */}
         </div>
         <div className="container d-flex justify-content-center">
           <button className="btn-principal-l mt-3" type="submit">Enviar</button>
         </div>
       </form>
     </div>
-    
   );
 };
 
--- src/hooks/useContactForm.js
+++ src/hooks/useContactForm.js
@@ -1,94 +1,70 @@
 import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import * as yup from 'yup';
+import {apiBancoProyecto} from '../services/bancoproyecto.api'; 
+import { yupResolver } from '@hookform/resolvers/yup';
 
 
-const useContactForm = (submitForm) => {
-    const [formData, setFormData] = useState({
-        full_name: '',
-        email: '',
-        organization: '',
-        contact_reason: '',
-        message: ''
+// Esquema de validación con Yup
+const schema = yup.object().shape({
+  full_name: yup.string().required('Por favor, introduce tu nombre completo.'),
+  email: yup.string().email('Formato de correo inválido.').required('Por favor, introduce tu correo electrónico.'),
+  organization: yup.string().required('Por favor, introduce tu organización.'),
+  contact_reason: yup.string().required('Por favor, indica la razón de tu contacto.'),
+  message: yup.string().required('Por favor, deja un comentario.')
+});
+
+const useContactForm = () => {
+  const { register, handleSubmit, formState: { errors } } = useForm({
+    resolver: yupResolver(schema)
+  });
+
+  const [formData, setFormData] = useState({
+    full_name: '',
+    email: '',
+    organization: '',
+    contact_reason: '',
+    message: ''
+  });
+
+  // Maneja el cambio en los inputs y actualiza el estado
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
     });
+  };
 
-    const [formErrors, setFormErrors] = useState({
-        full_nameError: '',
-        emailError: '',
-        organizationError: '',
-        contact_reasonError: '',
-        messageError: ''
+   // Maneja el envío del formulario
+  const onSubmit = async (data) => {
+    try {
+      const response = await apiBancoProyecto.post('contact/v1/', data);
+      if (response.status === 200) {
+        console.log('Form data submitted successfully:', response.data);
+        // Puedes agregar lógica adicional aquí, como mostrar un mensaje de éxito, redireccionar, etc.
+      } else {
+        console.log('Failed to submit form data:', response.data);
+      }
+    } catch (error) {
+      console.error('Error submitting form data:', error);
+      // Maneja los errores de manera adecuada, por ejemplo, mostrando un mensaje de error al usuario.
+    }
+    // Resetea el formulario
+    setFormData({
+      full_name: '',
+      email: '',
+      organization: '',
+      contact_reason: '',
+      message: ''
     });
+  };
 
-    const validateForm = () => {
-        let isValid = true;
-        const errors = {
-          full_nameError: '',
-          emailError: '',
-          organizationError: '',
-          contact_reasonError: '',
-          messageError: ''
-        };
-    
-        if (formData.full_name.trim() === '') {
-          errors.full_nameError = 'Por favor, introduce tu nombre completo.';
-          isValid = false;
-        }
-    
-        if (formData.email.trim() === '') {
-          errors.emailError = 'Por favor, introduce tu correo electrónico.';
-          isValid = false;
-        }
-    
-        if (formData.organization.trim() === '') {
-          errors.organizationError = 'Por favor, introduce tu organización.';
-          isValid = false;
-        }
-    
-        if (formData.contact_reason.trim() === '') {
-          errors.contact_reasonError = 'Por favor, indica la razón de tu contacto.';
-          isValid = false;
-        }
-    
-        if (formData.message.trim() === '') {
-          errors.messageError = 'Por favor, deja un comentario.';
-          isValid = false;
-        }
-    
-        setFormErrors(errors);
-        return isValid;
-    };
-
-    const handleChange = (e) => {
-        setFormData({
-            ...formData,
-            [e.target.name]: e.target.value
-        });
-    };
-
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        if (validateForm()) {
-            await submitForm(formData);
-            // Resetea el formulario
-             setFormData({
-                 full_name: '',
-                 email: '',
-                 organization: '',
-                 contact_reason: '',
-                 message: ''
-             });
-        } else {
-            console.log('Formulario inválido. Por favor, completa todos los campos.');
-        }
-    };
-
-    return {
-        formData,
-        formErrors,
-        setFormErrors,
-        validateForm,
-        handleChange,
-        handleSubmit
-    };
+  return {
+    register,
+    handleChange,
+    handleSubmit: handleSubmit(onSubmit),
+    errors
+  };
 };
 
 export default useContactForm;
