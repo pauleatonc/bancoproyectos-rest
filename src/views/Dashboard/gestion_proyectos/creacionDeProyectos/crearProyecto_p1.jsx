1e0f8f79a39c3bfd963cbdf081fcac9e10c1165c
Rosario Herrera Amunátegui <114428069+RosarioHA@users.noreply.github.com>
2023-09-25 17:38:27 -0300

    agrega avance en vista crear proyecto p1

--- src/views/Dashboard/gestion_proyectos/creacionDeProyectos/crearProyecto_p1.jsx
+++ src/views/Dashboard/gestion_proyectos/creacionDeProyectos/crearProyecto_p1.jsx
@@ -4,7 +4,28 @@ const CrearProyectoP1 = () => {
         <h2 className="text-sans-h2 mt-4 mb-5">Subir Proyecto: Banco de Proyectos</h2>
         
         <div className="col-10 mx-5">
-          <div className="d-flex justify-content-center border border-alert"> PASOS </div>
+
+          <div className="d-flex flex-column align-items-center">
+            <div className="d-flex flex-row align-items-center">
+              <div className="contador-circle-active text-center mx-1"><p className="mt-1">1</p></div>
+              <div className="contador-line-active"></div>
+              <div className="contador-line"></div>
+              <div className="contador-circle text-center mx-1">2</div>
+              <div className="contador-line"></div>
+              <div className="contador-line"></div>
+              <div className="contador-circle text-center mx-1">3</div>
+              <div className="contador-line"></div>
+              <div className="contador-line"></div>
+              <div className="contador-circle text-center mx-1">4</div>
+            </div>
+            <div className="col-9 d-flex flex-row justify-content-between mt-2">
+              <p className="text-sans-p"><strong>Información <br/> General</strong></p>
+              <p className="text-sans-p">Descripción, fotos <br/> y videos</p>
+              <p className="text-sans-p">Documentación</p>
+              <p className="text-sans-p">Vista previa</p>
+            </div>
+          </div>
+
           <form className="col">
             <div className="col-6 d-flex flex-column my-4">
               <label className="text-sans-p px-3">Elige el programa (Obligatorio)</label>
