5d7e923f848b5a24a96b219dd548fb247e4cc6ad
Rosario Herrera Amunátegui <114428069+RosarioHA@users.noreply.github.com>
2023-09-26 16:08:59 -0300

    agrega iconos, estilos de tipografias y estilos secciones imagenes

--- src/views/Dashboard/gestion_proyectos/creacionDeProyectos/crearInnovador_p1.jsx
+++ src/views/Dashboard/gestion_proyectos/creacionDeProyectos/crearInnovador_p1.jsx
@@ -5,33 +5,49 @@ const CrearProyectoInnovadorP1 = () => {
       <h2 className="text-sans-h2 mt-4 mb-5">Subir Proyecto: Proyectos Innovadores</h2>
 
       <div className="row">
-        <div className="col">
-          <div className="row">
-            Titulo
+        <div className="col-5">
+          <div className="d-flex flex-row justify-content-between my-4">
+            <h3 className="text-sans-h3">Título del Proyecto</h3>
+            <button>Editar <i className="material-symbols-rounded me-2">edit</i> </button>
           </div>
-          <div className="row">
-            Descripcion
+          <div className="d-flex flex-row justify-content-between my-4">
+            <h3 className="text-sans-h35">Descripción del proyecto</h3>
+            <button>Editar <i className="material-symbols-rounded me-2">edit</i> </button>
           </div>
-          <div className="row">
-            Fuentes
+          <div className="d-flex flex-row justify-content-between my-4">
+            <div className="d-flex flex-column">
+              <h3 className="text-sans-h35">Fuentes </h3>
+              <p className="text-sans-h5">(Opcional)</p>
+            </div>
+            <button className=""> <i className="material-symbols-rounded me-2">add</i> Agregar fuente</button>
           </div>
         </div>
 
-        <div className="col">
-          <div className="row">
-            Img Portada
+        <div className="col-6 ms-5">
+          <div className="">
+            <h3 className="text-sans-h35">Imagen de Portada</h3>
+            <div className="img-section-l my-3">
+              <i className="material-symbols-rounded me-2">add_a_photo</i>
+              <p className="text-sans-p">Agregar foto de portada</p>
+            </div>
           </div>
-          <div className="row">
-            Img Galeria
+
+          <div className="mt-5">
+            <h3 className="text-sans-h35">Imágenes para la galería</h3>
+            <p className="text-sans-h5">(Máximo 10 imágenes)</p>
+            <div className="img-section-s my-3">
+              <i className="material-symbols-rounded me-2">add_a_photo</i>
+              <p className="text-sans-p">Agregar fotos</p>
+            </div>
           </div>
         </div>
       </div>
 
-      <div>
-        <button>Enviar solicitud</button>
+      <div className="col-10 mt-5 d-flex justify-content-end">
+        <button>Enviar solicitud <i className="material-symbols-rounded me-2">arrow_forward_ios</i> </button>
       </div>
-      <div>
-        <button>Deshechar solicitud</button>
+      <div className="col-10 mt-5 d-flex justify-content-start">
+        <button>Deshechar solicitud <i className="material-symbols-rounded me-2">delete</i> </button>
       </div>
     </div>
   )
