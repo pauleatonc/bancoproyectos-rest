044e7bb27f612ce9ed1e9700635a75940f7b6942
Rosario Herrera Amunátegui <114428069+RosarioHA@users.noreply.github.com>
2023-09-26 14:45:37 -0300

    estructura vista crear proyectos innovadores - p1

--- src/views/Dashboard/gestion_proyectos/creacionDeProyectos/crearInnovador_p1.jsx
+++ src/views/Dashboard/gestion_proyectos/creacionDeProyectos/crearInnovador_p1.jsx
@@ -3,6 +3,36 @@ const CrearProyectoInnovadorP1 = () => {
   return (
     <div className="container view-container">
       <h2 className="text-sans-h2 mt-4 mb-5">Subir Proyecto: Proyectos Innovadores</h2>
+
+      <div className="row">
+        <div className="col">
+          <div className="row">
+            Titulo
+          </div>
+          <div className="row">
+            Descripcion
+          </div>
+          <div className="row">
+            Fuentes
+          </div>
+        </div>
+
+        <div className="col">
+          <div className="row">
+            Img Portada
+          </div>
+          <div className="row">
+            Img Galeria
+          </div>
+        </div>
+      </div>
+
+      <div>
+        <button>Enviar solicitud</button>
+      </div>
+      <div>
+        <button>Deshechar solicitud</button>
+      </div>
     </div>
   )
 }
