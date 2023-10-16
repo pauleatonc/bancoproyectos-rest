84c5fe0573c25fc9137c11c5d48c428e09c43329
Rosario Herrera Amunátegui <114428069+RosarioHA@users.noreply.github.com>
2023-10-16 17:20:54 -0300

    agrega elementos y estilos a vista evaluar innovadores

--- src/views/Dashboard/gestion_proyectos/evaluacionDeProyectos/evaluarInnovadores.jsx
+++ src/views/Dashboard/gestion_proyectos/evaluacionDeProyectos/evaluarInnovadores.jsx
@@ -10,7 +10,7 @@ const EvaluarInnovador = () => {
   };
 
   return (
-    <div className="container view-container ms-4">
+    <div className="container view-container ms-5">
       <h1 className="text-sans-h2 mb-3 mt-2">Proyectos Innovadores: Ver solicitud</h1>
       <button className="btn-secundario-s d-flex mb-4" onClick={handleBackButtonClick}>
         <i className="material-symbols-rounded me-2">arrow_back_ios</i>
@@ -18,23 +18,84 @@ const EvaluarInnovador = () => {
       </button>
 
       {/* Mensaje */}
-      <div className="conainer d-flex justify-content-center my-5">
-        <div className="tertiary-container d-flex col-9 p-3 px-5">
-          <div className="col-2 d-flex align-items-center">
+      <div className="d-flex justify-content-center my-5">
+        <div className="tertiary-container d-flex col-8 p-3">
+          <div className="col-1 d-flex align-items-center">
             <img className="" src={icon} />
           </div>
-          <div>
+          <div className="col ms-2">
             <h2 className="text-sans-h2">Esta solicitud está pendiente de evaluación</h2>
             <p className="text-sans-p">Revisa que está todo bien para enviar la solicitud de publicación al final de esta página.</p>
           </div>
         </div>
       </div>
 
-      <div>
+      <div className="d-flex my-5">
         {/* Descripcion del proyecto */}
-        <div></div>
+        <div className="dashed-container d-flex flex-column justify-content-between col-5 p-3">
+          <div className="">
+            <h3 className="text-sans-h2">Titulo - dinamico</h3>
+            <p className="text-sans-p"> Descripcion - dinamico</p>
+            <ul>
+              <li className="d-flex text-sans-p-tertiary">
+                <p className="text-decoration-underline">Visitar fuente 1</p>
+                <i className="material-symbols-rounded ms-2 pt-1">open_in_new</i>
+              </li>
+            </ul>
+          </div>
+
+          <div>COMPONENTE EVALUACION</div>
+        </div>
+
+        {/* Imagenes del proyecto */}
+        <div className="dashed-container d-flex flex-column justify-content-between ms-3 col-6 p-3">
+          <div>
+            <h3 className="text-sans-h35">Imagen de Portada</h3>
+            <div className="img-l">IMAGEN PORTADA</div>
+            <h3 className="text-sans-h35">Imágenes para la galería</h3>
+            <div className="img-s">MINIATURAS</div>
+          </div>
+
+          <div>COMPONENTE EVALUACION</div>
+        </div>
       </div>
-    
+
+     {/* Resumen evaluacion */}
+     <div className="col-11">
+        <h3 className="text-sans-h3">Evaluación de la solicitud</h3>
+        <p className="text-sans-p">Marcaste que estas secciones tienen problemas:</p>
+        <div>TABLA EVALUACION</div>
+
+        <div className="d-flex">
+          <p className="text-sans-p-tertiary"><strong>Por lo tanto la solicitud será:</strong></p>
+          <p className="text-sans-p ms-2 border">etiqueta</p>
+        </div>
+        <p className="text-sans-p-tertiary">Esta retroalimentación le llegará a $userName(solicitante), si crees que necesita más detalles para hacer las correcciones, puedes agregarlos a continuación.</p>
+
+        <div className="d-flex flex-column input-container mt-4">
+          <label className="text-sans-p input-label ms-3 ms-sm-0" htmlFor="message">Comentarios (Opcional)</label>
+          <textarea
+            className="input-l p-3"
+            id="message"
+            placeholder="Escribe un comentario adicional."
+          ></textarea>
+        </div>
+        <div className="d-flex justify-content-end">
+          <p>CONTADOR CARACTERES</p>
+        </div>
+
+        <div className="d-flex justify-content-between my-5">
+          <button className="btn-secundario-s d-flex"  onClick={handleBackButtonClick}>
+            <i className="material-symbols-rounded me-2">arrow_back_ios</i>
+            <p className="mb-0">Volver a Solicitudes de Proyectos</p>
+          </button>
+          <button className="btn-principal-s d-flex">
+            <p className="mb-0">Enviar evaluación</p>
+            <i className="material-symbols-rounded ms-2">arrow_forward_ios</i>
+          </button>
+        </div>
+      </div>
+
     </div>
   )
 }
