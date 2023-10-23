473afc262bd066387e36fba44f51d6ef600c582e
Rosario Herrera Amunátegui <114428069+RosarioHA@users.noreply.github.com>
2023-10-23 15:37:41 -0300

    implementa cambios en vista Evaluar Proyectos tambien

--- src/views/Dashboard/gestion_proyectos/evaluacionDeProyectos/evaluarProyecto.jsx
+++ src/views/Dashboard/gestion_proyectos/evaluacionDeProyectos/evaluarProyecto.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import icon from "../../../../static/img/icons/InfoBlue.svg";
 import EvaluarSeccion from "../../../../components/Dashboard/EvaluarSeccion";
@@ -10,6 +10,9 @@ const EvaluarProyecto = () => {
     imagenes: [],
     documentos: [],
   });
+  const [mostrarResumen, setMostrarResumen] = useState(false);
+  const [allAccepted, setAllAccepted] = useState(false);
+  const [anyNotAccepted, setAnyNotAccepted] = useState(false);
 
   // Maneja boton de volver atras
   const history = useNavigate();
@@ -36,12 +39,28 @@ const EvaluarProyecto = () => {
     { value: 'Video del proyecto con problemas', label: 'Video del proyecto con problemas' },
   ];
 
-  // Opciones EvaluarSeccion Imagenes
+  // Opciones EvaluarSeccion Documentos
   const opcionesEvaluarDocs = [
     { value: 'Especificaciones Técnicas incorrectas o con errores', label: 'Especificaciones Técnicas incorrectas o con errores' },
     { value: 'Presupuesto incorrecto o con errores', label: 'Presupuesto incorrecto o con errores' },
     { value: 'Otros documentos del proyecto con errores', label: 'Otros documentos del proyecto con errores' },
   ];
+
+    // Maneja calificacion "no" en EvaluarSeccion
+    const handleReject = () => {
+      setAnyNotAccepted(true);
+      setMostrarResumen(true);
+    };
+  
+    useEffect(() => {
+      const contenidoAccepted = todasLasSelecciones.contenido.every((seleccion) => seleccion === 'Si');
+      const imagenesAccepted = todasLasSelecciones.imagenes.every((seleccion) => seleccion === 'Si');
+      setAllAccepted(contenidoAccepted && imagenesAccepted);
+  
+      if (contenidoAccepted && imagenesAccepted) {
+        setMostrarResumen(false);
+      }
+    }, [todasLasSelecciones]);
 
   return (
     <div className="container view-container ms-4">
@@ -82,7 +101,9 @@ const EvaluarProyecto = () => {
               ...prevSelecciones,
               contenido: seleccionados,
             }));
-          }}/>
+          }}
+          onReject = {handleReject}
+          />
         </div>
         
       </div>
@@ -124,7 +145,9 @@ const EvaluarProyecto = () => {
               ...prevSelecciones,
               imagenes: seleccionados,
             }));
-          }}/>
+          }}
+          onReject = {handleReject}
+          />
         </div>
       </div>
 
@@ -149,98 +172,94 @@ const EvaluarProyecto = () => {
               ...prevSelecciones,
               documentos: seleccionados,
             }));
-          }}/>
+          }}
+          onReject = {handleReject}
+          />
         </div>
       </div>
 
       {/* Resumen evaluacion */}
       <div className="col-11 mx-5">
-        <h3 className="text-sans-h3-tertiary">Evaluación de la solicitud</h3>
-        <p className="text-sans-p-tertiary">Marcaste que estas secciones tienen problemas:</p>
-
-        <div className="container row mt-4 mb-5">
-          {todasLasSelecciones.contenido.length > 0 && (
-          <div className="col-4">
-            <div>
-              <p className="text-sans-p ms-3">Sección 1</p>
-              <div>
-                {todasLasSelecciones.contenido.map((seleccion, index) => (
-                  <div 
-                  key={seleccion} 
-                  className={`d-flex py-4 text-sans-h5-red ${index % 2 === 0 ? 'grey-table-line' : 'white-table-line'}`}
-                  >
-                    <i className="material-symbols-rounded ms-3">warning</i>
-                    <p className="text-sans-p ms-4 mb-0">{seleccion}</p>
+        {mostrarResumen && (anyNotAccepted || !allAccepted) && (
+        <div>
+          <h3 className="text-sans-h3-tertiary">Evaluación de la solicitud</h3><p className="text-sans-p-tertiary">Marcaste que estas secciones tienen problemas:</p><div className="container row mt-4 mb-5">
+              {todasLasSelecciones.contenido.length > 0 && (
+                <div className="col-4">
+                  <div>
+                    <p className="text-sans-p ms-3">Sección 1</p>
+                    <div>
+                      {todasLasSelecciones.contenido.map((seleccion, index) => (
+                        <div
+                          key={seleccion}
+                          className={`d-flex py-4 text-sans-h5-red ${index % 2 === 0 ? 'grey-table-line' : 'white-table-line'}`}
+                        >
+                          <i className="material-symbols-rounded ms-3">warning</i>
+                          <p className="text-sans-p ms-4 mb-0">{seleccion}</p>
+                        </div>
+                      ))}
+                    </div>
                   </div>
-                ))}
-              </div>
-            </div>
-          </div>
-          )}
-          {todasLasSelecciones.imagenes.length > 0 && (
-          <div className="col-4">
-            <div>
-              <p className="text-sans-p ms-3">Sección 2</p>
-              <div>
-                {todasLasSelecciones.imagenes.map((seleccion, index) => (
-                  <div 
-                  key={seleccion} 
-                  className={`d-flex py-4 text-sans-h5-red ${index % 2 === 0 ? 'grey-table-line' : 'white-table-line'}`}
-                  >
-                    <i className="material-symbols-rounded ms-2 red-icon">warning</i>
-                    <p className="text-sans-p ms-4 mb-0">{seleccion}</p>
                 </div>
-                ))}
-              </div>
-            </div>
-          </div>
-          )}
-          {todasLasSelecciones.imagenes.length > 0 && (
-          <div className="col-4">
-            <div>
-              <p className="text-sans-p ms-3">Sección 3</p>
-              <div>
-                {todasLasSelecciones.documentos.map((seleccion, index) => (
-                  <div 
-                  key={seleccion} 
-                  className={`d-flex py-4 text-sans-h5-red ${index % 2 === 0 ? 'grey-table-line' : 'white-table-line'}`}
-                  >
-                    <i className="material-symbols-rounded ms-2 red-icon">warning</i>
-                    <p className="text-sans-p ms-4 mb-0">{seleccion}</p>
+              )}
+              {todasLasSelecciones.imagenes.length > 0 && (
+                <div className="col-4">
+                  <div>
+                    <p className="text-sans-p ms-3">Sección 2</p>
+                    <div>
+                      {todasLasSelecciones.imagenes.map((seleccion, index) => (
+                        <div
+                          key={seleccion}
+                          className={`d-flex py-4 text-sans-h5-red ${index % 2 === 0 ? 'grey-table-line' : 'white-table-line'}`}
+                        >
+                          <i className="material-symbols-rounded ms-2 red-icon">warning</i>
+                          <p className="text-sans-p ms-4 mb-0">{seleccion}</p>
+                        </div>
+                      ))}
+                    </div>
+                  </div>
                 </div>
-                ))}
+              )}
+              {todasLasSelecciones.documentos.length > 0 && (
+                <div className="col-4">
+                  <div>
+                    <p className="text-sans-p ms-3">Sección 3</p>
+                    <div>
+                      {todasLasSelecciones.documentos.map((seleccion, index) => (
+                        <div
+                          key={seleccion}
+                          className={`d-flex py-4 text-sans-h5-red ${index % 2 === 0 ? 'grey-table-line' : 'white-table-line'}`}
+                        >
+                          <i className="material-symbols-rounded ms-2 red-icon">warning</i>
+                          <p className="text-sans-p ms-4 mb-0">{seleccion}</p>
+                        </div>
+                      ))}
+                    </div>
+                  </div>
+                </div>
+              )}
+              {/* Mensaje si no hay selecciones */}
+              {todasLasSelecciones.contenido.length === 0 && todasLasSelecciones.imagenes.length === 0 && todasLasSelecciones.documentos.length === 0 && (
+                <div className="col-8">
+                  <p className="text-sans-p-bold-darkred ms-3">Debes justificar tu evaluación en cada sección rechazada.</p>
+                </div>
+              )}
+            </div><div className="d-flex">
+                <p className="text-sans-p-tertiary"><strong>Por lo tanto la solicitud será:</strong></p>
+                <p className="text-sans-p ms-2 border ms-5">etiqueta</p>
+              </div><p className="text-sans-p-tertiary">Esta retroalimentación le llegará a $userName(solicitante), si crees que necesita más detalles para hacer las correcciones, puedes agregarlos a continuación.</p><div className="d-flex flex-column input-container mt-4">
+                <label className="text-sans-p input-label ms-3 ms-sm-0" htmlFor="message">Comentarios (Opcional)</label>
+                <textarea
+                  className="input-l p-3"
+                  id="message"
+                  placeholder="Escribe un comentario adicional."
+                  value={comentario}
+                  onChange={(e) => setComentario(e.target.value)}
+                ></textarea>
+              </div><div className="d-flex justify-content-end">
+                <p className="text-sans-h5">{comentario.length} / 200 caracteres</p>
               </div>
             </div>
-          </div>
-          )}
-          {/* Mensaje si no hay selecciones */}
-          {todasLasSelecciones.contenido.length === 0 && todasLasSelecciones.imagenes.length === 0 && todasLasSelecciones.documentos.length === 0 && (
-            <div className="col-8">
-              <p className="text-sans-p ms-3">Aún no has detectado problemas en ninguna de las secciones.</p>
-            </div>
-          )}
-        </div>
-
-        <div className="d-flex">
-          <p className="text-sans-p-tertiary"><strong>Por lo tanto la solicitud será:</strong></p>
-          <p className="text-sans-p ms-2 border ms-5">etiqueta</p>
-        </div>
-
-        <p className="text-sans-p-tertiary">Esta retroalimentación le llegará a $userName(solicitante), si crees que necesita más detalles para hacer las correcciones, puedes agregarlos a continuación.</p>
-
-        <div className="d-flex flex-column input-container mt-4">
-          <label className="text-sans-p input-label ms-3 ms-sm-0" htmlFor="message">Comentarios (Opcional)</label>
-          <textarea
-            className="input-l p-3"
-            id="message"
-            placeholder="Escribe un comentario adicional."
-            value={comentario}
-            onChange={(e) => setComentario(e.target.value)}
-          ></textarea>
-        </div>
-        <div className="d-flex justify-content-end">
-          <p className="text-sans-h5">{comentario.length} / 200 caracteres</p>
-        </div>
+        )}
 
         <div className="d-flex justify-content-between my-5">
           <button className="btn-secundario-s d-flex"  onClick={handleBackButtonClick}>
