423dc474de268a4949ec6d1b64e26fae78c81d35
JaimeHernandezE <jhearquitecto@gmail.com>
2024-03-12 19:16:46 -0300

    ADD: Restricciones para crear, eliminar y editar solo administradores

--- applications/good_practices/api/v1/goodPracticesViewSet.py
+++ applications/good_practices/api/v1/goodPracticesViewSet.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.exceptions import PermissionDenied
 #
 from applications.good_practices.models import (
     GoodPractices,
@@ -8,7 +9,24 @@ from .goodPracticesSerializer import (
     GoodPracticesSerializerV1
 )
 
+from applications.users.permissions import is_admin
+
 
 class GoodPracticesViewSet(viewsets.ModelViewSet):
     serializer_class = GoodPracticesSerializerV1
     queryset = GoodPractices.objects.filter(public=True)
+
+    def create(self, request, *args, **kwargs):
+        if not is_admin(request.user):
+            raise PermissionDenied(detail="No tienes permiso para realizar esta acción.")
+        return super().create(request, *args, **kwargs)
+
+    def update(self, request, *args, **kwargs):
+        if not is_admin(request.user):
+            raise PermissionDenied(detail="No tienes permiso para realizar esta acción.")
+        return super().update(request, *args, **kwargs)
+
+    def destroy(self, request, *args, **kwargs):
+        if not is_admin(request.user):
+            raise PermissionDenied(detail="No tienes permiso para realizar esta acción.")
+        return super().destroy(request, *args, **kwargs)
--- applications/projects/api/v1/typesViewSet.py
+++ applications/projects/api/v1/typesViewSet.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.exceptions import PermissionDenied
 #
 from applications.projects.models import (
     Type,
@@ -7,8 +8,24 @@ from applications.projects.models import (
 from .projectSerializer import (
     TypeSerializerV1,
 )
+from applications.users.permissions import is_admin
 
 
 class ProgramViewSet(viewsets.ModelViewSet):
     serializer_class = TypeSerializerV1
     queryset = Type.objects.all()
+
+    def create(self, request, *args, **kwargs):
+        if not is_admin(request.user):
+            raise PermissionDenied(detail="No tienes permiso para realizar esta acción.")
+        return super().create(request, *args, **kwargs)
+
+    def update(self, request, *args, **kwargs):
+        if not is_admin(request.user):
+            raise PermissionDenied(detail="No tienes permiso para realizar esta acción.")
+        return super().update(request, *args, **kwargs)
+
+    def destroy(self, request, *args, **kwargs):
+        if not is_admin(request.user):
+            raise PermissionDenied(detail="No tienes permiso para realizar esta acción.")
+        return super().destroy(request, *args, **kwargs)
