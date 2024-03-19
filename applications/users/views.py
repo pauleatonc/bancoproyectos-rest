e2ea9359dc67731cf3cb45773e3112b4dda9f25a
Paul Eaton <paul.eaton@subdere.gov.cl>
2024-03-19 08:51:08 -0300

    WIP: Adding keycloak

--- applications/users/views.py
+++ applications/users/views.py
@@ -13,6 +13,9 @@ from rest_framework_simplejwt.tokens import RefreshToken
 #
 from applications.users.api.v1.serializers import CustomTokenObtainPairSerializer, UserSerializer
 from applications.users.models import User
+# keycloak configuration
+from django.http import JsonResponse
+from .keycloak_auth import verify_or_create_user
 
 
 class Login(TokenObtainPairView):
@@ -47,3 +50,14 @@ class Logout(APIView):
             RefreshToken.for_user(user.first())
             return Response({'message': 'Sesión cerrada correctamente.'}, status=status.HTTP_200_OK)
         return Response({'error': 'No existe este usuario.'}, status=status.HTTP_400_BAD_REQUEST)
+
+
+#keycloak configuration
+def keycloak_login(request):
+    token = request.GET.get('token')  # Asumiendo que el token se envía como parte de la solicitud
+    user = verify_or_create_user(token)
+    if user:
+        # Manejar el inicio de sesión del usuario, generar tu propio token para la sesión, etc.
+        return JsonResponse({'message': 'Usuario autenticado con éxito'})
+    else:
+        return JsonResponse({'message': 'Error de autenticación'}, status=401)
