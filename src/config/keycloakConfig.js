d3d53191b387561105cf1e2310573bc9b839c0cb
Paul Eaton <paul.eaton@subdere.gov.cl>
2024-03-22 16:55:42 -0300

    WIP keycloak configuration

--- src/config/keycloakConfig.js
+++ src/config/keycloakConfig.js
@@ -4,6 +4,7 @@ const keycloakConfig = {
   url: 'https://oid.subdere.gob.cl/', // La URL del servidor de Keycloak
   realm: 'app-qa', // El nombre del realm
   clientId: 'bancoproyectos', // El clientId configurado en Keycloak
+  scope: 'openid profile claveUnica',
 };
 
 const keycloak = new Keycloak(keycloakConfig);
